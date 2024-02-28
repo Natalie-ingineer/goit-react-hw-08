5ec50612b93bb7ece5ae2a0af8590cd9ad917e2a
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-02-28 10:14:53 +0200

    add folders pages, redux, hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { Provider } from "react-redux";
+import { App } from "./components/App";
+import { store, persistor } from "./redux/store";
 import "modern-normalize";
 import "./index.css";
-import { Provider } from "react-redux";
-import { store } from "./redux/store.js";
-// import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
