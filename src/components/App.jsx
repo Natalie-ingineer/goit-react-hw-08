c6a9df62afe0eb637b2ce63e833216df39a4624a
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-02-28 10:00:21 +0200

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,50 @@
-import ContactForm from "./ContactForm/ContactForm";
-import ContactList from "./ContactList/ContactList";
-import SearchBox from "./SearchBox/SearchBox";
+import { useEffect, lazy } from "react";
+import { useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
+import { Layout } from "./Layout";
+import { PrivateRoute } from "./PrivateRoute";
+import { RestrictedRoute } from "./RestrictedRoute";
+import { refreshUser } from "../redux/auth/operations";
+import { useAuth } from "../hooks";
 
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchContacts } from "../redux/operations";
-import { selectContacts, selectLoading, selectError } from "../redux/selectors";
+const HomePage = lazy(() => import("../pages/Home"));
+const RegisterPage = lazy(() => import("../pages/Register"));
+const LoginPage = lazy(() => import("../pages/Login"));
+const TasksPage = lazy(() => import("../pages/Tasks"));
 
-export default function App() {
+export const App = () => {
   const dispatch = useDispatch();
-
-  const items = useSelector(selectContacts);
-
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
+  const { isRefreshing } = useAuth();
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
-  return (
-    <>
-      <h1>Phonebook</h1>
-
-      {loading && <p>Loading...</p>}
-      {error && <p className="notification">Oops, ERROR 😨</p>}
-
-      {items.length > 0 && JSON.stringify(items, null, 2) && (
-        <>
-          <ContactForm />
-          <SearchBox />
-          <ContactList />
-        </>
-      )}
-    </>
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/register"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<RegisterPage />} />
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <RestrictedRoute redirectTo="/tasks" component={<LoginPage />} />
+          }
+        />
+        <Route
+          path="/tasks"
+          element={
+            <PrivateRoute redirectTo="/login" component={<TasksPage />} />
+          }
+        />
+      </Route>
+    </Routes>
   );
-}
+};
