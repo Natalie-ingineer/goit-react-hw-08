c7d7a75c3ff82a74293509514fe9d11d576b400d
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-04 11:08:12 +0200

    remuve Fuse.js

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,4 +1,3 @@
-import Fuse from "fuse.js";
 import css from "./SearchBox.module.css";
 import { useId, useState } from "react";
 import { nameFilter, numberFilter } from "../../redux/contacts/filterSlice";
@@ -7,19 +6,8 @@ import { useDispatch, useSelector } from "react-redux";
 export default function SearchBox() {
   const usernameFieldIdsearch = useId();
   const dispatch = useDispatch();
-  const [contacts, setContacts] = useState([]);
+
   const [searchTerm, setSearchTerm] = useState("");
-
-  const fuse = new Fuse(contacts, {
-    key: ["name", "number"],
-  });
-
-  const results =
-    contacts.length > 0 && searchTerm !== ""
-      ? fuse.search(searchTerm)
-      : contacts;
-  const searchValue = results.map((result) => result.item);
-  console.log(searchValue);
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
