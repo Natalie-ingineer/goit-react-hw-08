57d716cfcb4cc9ffbee63cb6ce2ee4175bb8e7bc
Natalie-ingineer <138399057+Natalie-ingineer@users.noreply.github.com>
2024-03-04 22:46:04 +0200

    Update EditForm.jsx

--- src/components/EditForm/EditForm.jsx
+++ src/components/EditForm/EditForm.jsx
@@ -1,25 +1,87 @@
-import css from "./EditForm.module.css";
-import { RiSaveLine } from "react-icons/ri";
-import { MdOutlineCancel } from "react-icons/md";
-import { updateCurrentContact } from "../../redux/contacts/operations";
-// import { selectCurrentContact } from "../../redux/contacts/selectors";
-import { useDispatch, useSelector } from "react-redux";
-import { addCurrentContact } from "../../redux/contacts/contactSlice";
+// import css from "./EditForm.module.css";
+// import { RiSaveLine } from "react-icons/ri";
+// import { MdOutlineCancel } from "react-icons/md";
+// import { updateCurrentContact } from "../../redux/contacts/operations";
+// // import { selectCurrentContact } from "../../redux/contacts/selectors";
+// import { useDispatch, useSelector } from "react-redux";
+// import { addCurrentContact } from "../../redux/contacts/contactSlice";
+
+// // export default function EditForm() {
+// //   const currentContactName = useSelector(
+// //     (state) => state.contacts.currentContact.name
+// //   );
+// //   // console.log(currentContactName);
+// //   const currentContactNumber = useSelector(
+// //     (state) => state.contacts.currentContact.number
+// //   );
+// //   // console.log(currentContactNumber);
+
+// //   const currentContactId = useSelector(
+// //     (state) => state.contacts.currentContact.id
+// //   );
+// //   // console.log(currentContactId);
+
+// //   const dispatch = useDispatch();
+
+// //   const handleSubmit = (e) => {
+// //     e.preventDefault();
+// //     const valueName = e.target.elements.name.value;
+// //     console.log(valueName);
+// //     const valueNumber = e.target.elements.number.value;
+// //     console.log(valueNumber);
+// //     const valueId = e.target.elements.id.value;
+// //     console.log(valueId);
+// //     dispatch(updateCurrentContact({ valueName, valueNumber, valueId }));
+// //     dispatch(addCurrentContact(null));
+// //   };
+
+// //   return (
+// //     <form className={css.form} onChange={handleSubmit}>
+// //       <input
+// //         className={css.input}
+// //         placeholder="What do you want to write?"
+// //         name="name"
+// //         required
+// //         defaultValue={currentContactName}
+// //         autoFocus
+// //       />
+
+// //       <input
+// //         className={css.input}
+// //         onChange={handleSubmit}
+// //         placeholder="What do you want to write?"
+// //         name="number"
+// //         required
+// //         defaultValue={currentContactNumber}
+// //         autoFocus
+// //       />
+// //       <button className={css.submitButton} type="submit">
+// //         <RiSaveLine color="green" size="16px" />
+// //       </button>
+
+// //       <button
+// //         className={css.editButton}
+// //         type="button"
+// //         onClick={() => dispatch(addCurrentContact(null))}
+// //       >
+// //         <MdOutlineCancel color="red" size="16px" />
+// //       </button>
+// //     </form>
+// //   );
+// // }
 
 // export default function EditForm() {
 //   const currentContactName = useSelector(
 //     (state) => state.contacts.currentContact.name
 //   );
-//   // console.log(currentContactName);
+
 //   const currentContactNumber = useSelector(
 //     (state) => state.contacts.currentContact.number
 //   );
-//   // console.log(currentContactNumber);
 
 //   const currentContactId = useSelector(
 //     (state) => state.contacts.currentContact.id
 //   );
-//   // console.log(currentContactId);
 
 //   const dispatch = useDispatch();
 
@@ -31,34 +93,44 @@ import { addCurrentContact } from "../../redux/contacts/contactSlice";
 //     console.log(valueNumber);
 //     const valueId = e.target.elements.id.value;
 //     console.log(valueId);
-//     dispatch(updateCurrentContact({ valueName, valueNumber, valueId }));
-//     dispatch(addCurrentContact(null));
+
+//     if (
+//       valueName !== currentContactName ||
+//       valueNumber !== currentContactNumber
+//     ) {
+//       dispatch(
+//         updateCurrentContact({
+//           name: valueName,
+//           number: valueNumber,
+//           id: valueId,
+//         })
+//       );
+//     } else {
+//       dispatch(addCurrentContact(null));
+//     }
 //   };
 
 //   return (
-//     <form className={css.form} onChange={handleSubmit}>
+//     <form className={css.form} onSubmit={handleSubmit}>
 //       <input
 //         className={css.input}
-//         placeholder="What do you want to write?"
+//         placeholder="Name"
 //         name="name"
 //         required
 //         defaultValue={currentContactName}
 //         autoFocus
 //       />
-
 //       <input
 //         className={css.input}
-//         onChange={handleSubmit}
-//         placeholder="What do you want to write?"
+//         placeholder="Phone Number"
 //         name="number"
 //         required
 //         defaultValue={currentContactNumber}
-//         autoFocus
 //       />
+//       <input type="hidden" name="id" value={currentContactId} />
 //       <button className={css.submitButton} type="submit">
 //         <RiSaveLine color="green" size="16px" />
 //       </button>
-
 //       <button
 //         className={css.editButton}
 //         type="button"
@@ -69,6 +141,13 @@ import { addCurrentContact } from "../../redux/contacts/contactSlice";
 //     </form>
 //   );
 // }
+
+import css from "./EditForm.module.css";
+import { RiSaveLine } from "react-icons/ri";
+import { MdOutlineCancel } from "react-icons/md";
+import { updateCurrentContact } from "../../redux/contacts/operations";
+import { useDispatch, useSelector } from "react-redux";
+import { addCurrentContact } from "../../redux/contacts/contactSlice";
 
 export default function EditForm() {
   const currentContactName = useSelector(
